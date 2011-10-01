4c44f39cfd5a71faf763658936ae866a9dbf446a
xtine <piroko@gmail.com>
2011-09-30 20:35:54 -0700

    change ordering of blog posts

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.template import RequestContext, Context
 from blog.models import Entry
 
 def index(request):
-    entries = Entry.objects.all().order_by('published')
+    entries = Entry.objects.all().order_by('-published')
     context = RequestContext(request)
     return render_to_response('blog/index.html', {'entries' : entries}, context_instance=context)
