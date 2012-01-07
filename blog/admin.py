db143348ab30120d67c9e2f150b91ca7a5f2dd9e
Christine Cheung <piroko@gmail.com>
2012-01-07 01:16:27 -0800

    Set admin templates to use TinyMCE.

--- blog/admin.py
+++ blog/admin.py
@@ -6,12 +6,5 @@ class EntryAdmin(admin.ModelAdmin):
     list_display_links = ['title']
     prepopulated_fields = {'slug' : ('title',)}
 
-    # TinyMCE
-    class Media:
-        js = [
-            '/static/grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js',
-            '/static/grappelli/tinymce_setup/tinymce_setup.js',
-        ]
-
 
 admin.site.register(Entry, EntryAdmin)
