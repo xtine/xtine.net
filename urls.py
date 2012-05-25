651e38fae75353d0aa33ee1c71577b8ef5cedeee
Christine Cheung <piroko@gmail.com>
2012-05-24 22:04:46 -0700

    PEP8

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^blog/$', 'blog.views.index'),
     (r'^blog/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'blog.views.detail'),
-    
+
     (r'^$', direct_to_template, {'template': 'index.html'}),
 
     (r'^contact/', include('contact_form.urls')),
@@ -20,8 +20,8 @@ urlpatterns = patterns('',
 if settings.LOCAL_MEDIA:
     urlpatterns += patterns('',
         url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root' : settings.STATIC_ROOT + "/",
-            'show_indexes' : True
+            'document_root': settings.STATIC_ROOT + "/",
+            'show_indexes': True
         }),
     )
 
