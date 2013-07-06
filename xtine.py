64a7153e5cde59f63f98c30978f010edda153219
xtine <piroko@gmail.com>
2013-07-05 21:26:04 -0700

    clean up app file, add more templates

--- xtine.py
+++ xtine.py
@@ -1,51 +1,25 @@
-# all the imports
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash, _app_ctx_stack
-from contextlib import closing
+from flask import Flask, render_template
 
 
-# configuration
-DATABASE = 'db/blog.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
-# create our little application :)
+# let there be Flask
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
-def init_db():
-    """Creates the database tables."""
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        sqlite_db.row_factory = sqlite3.Row
-        top.sqlite_db = sqlite_db
-
-    return top.sqlite_db
-
-
 @app.route('/')
-def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('index.html', entries=entries)
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', url='about')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', url='projects')
 
 
 if __name__ == '__main__':
