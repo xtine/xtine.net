03a76fbc9927d073bc8bd54ae8b54225f16a54f5
xtine <piroko@gmail.com>
2013-07-06 02:12:01 -0700

    set index active state

--- xtine.py
+++ xtine.py
@@ -7,7 +7,7 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', url='index')
 
 
 @app.route('/projects')
