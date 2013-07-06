4993d56e96c8f24170b36491e881b1e242902fbb
xtine <piroko@gmail.com>
2013-07-06 02:04:53 -0700

    remove pages, make sekrit projects page

--- xtine.py
+++ xtine.py
@@ -1,20 +1,13 @@
 from flask import Flask, render_template
 
-
 # let there be Flask
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html', url='about')
 
 
 @app.route('/projects')
