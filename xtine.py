ba7b46c0d7a1ebc07897d1444ed983641a39e174
xtine <piroko@gmail.com>
2013-07-06 02:24:35 -0700

    contact page

--- xtine.py
+++ xtine.py
@@ -27,6 +27,11 @@ def projects():
     return render_template('projects.html', url='projects')
 
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', url='contact')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
