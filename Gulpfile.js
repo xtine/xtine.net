89defe9ca57f86b29fd34b1d1f43ecbaebbbdc35
xtine <christine@fieryorange.com>
2017-01-03 22:51:25 -0700

    minify css

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     neat = require('node-neat').includePaths,
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
     watch = require('gulp-watch');
 
 gulp.task('sass', function() {
@@ -8,6 +10,8 @@ gulp.task('sass', function() {
     .pipe(sass({
       includePaths: ['styles'].concat(neat)
     }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
     .pipe(gulp.dest('css'))
 });
 
